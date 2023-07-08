02a288aeafe99bd84645fde9a7b8c28af7738f02
Shashank3736 <shreyashr267@gmail.com>
2023-07-08 18:01:44 +0530

    completed

--- python/909.py
+++ python/909.py
@@ -9,29 +9,26 @@ class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
         board.reverse()
-        def intToPos(i) -> [int, int]:
-            i = i-1
-            r = i // n
-            c = i%n
 
-            if r%2:
+        def intToPos(i):
+            r = (i - 1) // n
+            c = (i - 1) % n
+            if r % 2:
                 # even row
                 c = n - 1 - c
-            
             return [r, c]
         
         q = deque()
         q.append([1, 0]) # [pos, moves]
         visit = set()
-
         while q:
             pos, moves = q.popleft()
-
-            for i in range(pos+1, pos+7):
-                [r, c] = intToPos(i)
+            for i in range(1, 7):
+                npos = pos + i
+                r, c = intToPos(npos)
                 if board[r][c] != -1:
-                    i = board[r][c]
-                if board[r][c] == n*n:
+                    npos = board[r][c]
+                if npos == n*n:
                     return moves + 1
                 if i not in visit:
                     visit.add(i)
